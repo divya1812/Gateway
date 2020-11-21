56f528be032c028e41b014e0566f45580291cf71
divya1812 <chhabariadivyagmail.com>
2020-11-22 00:45:13 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,6 +138,23 @@
     });
   });
 
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 6
+      }
+    }
+  });
+  
   $(".portfolio-details-carousel").owlCarousel({
     autoplay: true,
     dots: true,
