a4f3288e9108c048336ab28e8d117f65c896cd24
divya1812 <chhabariadivyagmail.com>
2021-04-08 23:09:29 +0530

    Material Ui Components

---
+++ React_Etest/todo/src/components/inputtodo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Inputtodo = () => {
+    return (
+        <div>
+            //textfield
+            //send button
+            //usestate
+        </div>
+    )
+}
---
+++ React_Etest/todo/src/components/todolist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Todolist = () => {
+    return (
+        <div>
+           //use selector to fetch Todolist
+           //map the todo list and render the Todolist
+           //app bar
+        </div>
+    )
+}
